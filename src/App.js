4d91105240f2a2a5f853804a90ebd7136f6d389f
bak <bakman152@gmail.com>
2020-01-24 11:20:32 +0500

    все сломал и починил

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.sass';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fab} from '@fortawesome/free-brands-svg-icons'
+import {
+    faArrowLeft, faArrowRight,
+    faBaby, faCamera, faCheck, faCommentAlt,
+    faEnvelope,
+    faExclamation, faExclamationCircle, faHeart,
+    faHome,
+    faMapMarkerAlt, faMedal, faMoneyBill,
+    faNewspaper, faRubleSign, faSleigh, faSlidersH,
+    faUser, faVideo,
+} from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {BrowserRouter, Route} from "react-router-dom";
+import MyPageAllCOmp from "./components/MyPage/MyPageAllCOmp";
+import NewsPage from "./components/_NewsPage/NewsPage";
+import Navbar from "./components/NavBar/Navbar";
+import Musora from "./components/Musora/Musora";
+import SalePage from "./components/SalePage/SalePage";
+import Mymessage from "./components/Mymessage/Mymessage";
+import Conversation from "./components/Сonversation/Conversation";
+
+
+library.add(fab, faHome, faNewspaper, faEnvelope,
+    faExclamation, faMapMarkerAlt, faUser, faSlidersH, faBaby, faMedal,
+    faHeart, faCommentAlt, faExclamationCircle, faSleigh, faCamera, faVideo,
+    faMoneyBill, faArrowLeft, faArrowRight, faRubleSign,faCheck);
+
+
+let App = ( ) => {
+
+    return (
+        <BrowserRouter>
+            <div className="navbar_position">
+                <div className='item'>
+                    < Navbar/>
+                </div>
+                {/*Компоненты главной страницы*/}
+                <Route path='/mypage' render={() => <MyPageAllCOmp/>}/>
+
+                {/*Компоненты главной страницы*/}
+
+                {/*компоненты новостей*/}
+                <Route path='/news' render={() => <NewsPage/>}/>
+                {/*компоненты новостей*/}
+
+                {/*компоненты блока сообщений*/}
+                <Route path='/message'
+                       render={() => <Mymessage/>}/>
+                {/*компоненты блока сообщений*/}
+
+                {/*components block for gde stoyat*/}
+                <Route path='/gdesuki' render={() => <Musora/>}/>
+
+
+                {/*components block for gde stoyat*/}
+
+
+                <Route path='/sale' render={() => <SalePage  />}/>
+
+
+                <Route path='/settings' render={() => <Conversation  />}/>
+
+            </div>
+
+        </BrowserRouter>
+
+    )
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
