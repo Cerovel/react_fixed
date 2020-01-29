f3b815754f677a16651f1dd116987df000182035
bak <bakman152@gmail.com>
2020-01-29 09:45:24 +0500

    я выполнил урок 49. создал reducer и при помощи connect связал все

--- src/components/Сonversation/ParticipantContainer/Participant/Participant.jsx
+++ src/components/Сonversation/ParticipantContainer/Participant/Participant.jsx
@@ -29,15 +29,3 @@ const Participant = (props) => {
 export default Participant
 
 
-
-// props.setUsers(
-//     [
-//         {id: 1, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: false, avatar: '', fullName: 'sanek'},
-//         {id: 2, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: true,  avatar: '', fullName: 'dasha'},
-//         {id: 3, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: false, avatar: '', fullName: 'masha'},
-//         {id: 4, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: true, avatar: '', fullName: 'masha'},
-//         {id: 5, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: true, avatar: '', fullName: 'masha'},
-//         {id: 6, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: false, avatar: '', fullName: 'masha'},
-//         {id: 7, photo_url: 'https://pm1.narvii.com/6935/4d5297bae7b4e20b0ade8edc0c0f1d79355b1a95r1-768-1064v2_128.jpg', followed: false, avatar: '', fullName: 'masha'},
-//     ]
-// );
